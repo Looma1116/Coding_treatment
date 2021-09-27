88ae8b20ae4ffb77b969609962e5cfaae3b34b87
Looma1116 <jksjsmlove2@naver.com>
2021-09-27 17:55:00 +0900

    ASda

---
+++ Algorithm/2021autumn/week4_1.cpp
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int H[101] = {0};
+int n = 0;
+
+
+void downHeap(int i){
+    if(i*2 > n && i*2+1 > n){
+        return;
+    }
+        int big = i * 2;
+        if ((i * 2 + 1) <= n)
+        {
+            if (H[i * 2 + 1] > H[big])
+            {
+                big = i * 2 + 1;
+            }
+    }
+    if(H[i]>= H[big]){
+        return;
+    }
+    int tmp = H[i];
+    H[i] = H[big];
+    H[big] = tmp;
+    downHeap(big);
+}
+
+//재귀
+void rBuildHeap(int i){
+    if (i > n){
+        return;
+    }
+    rBuildHeap(2*i);
+    rBuildHeap(2*i + 1);
+    downHeap(i);
+    return;
+}
+
+//비재귀
+void buildHeap(){
+    for (int i = n / 2; i > 0;i--){
+        downHeap(i);
+    }
+    return;
+}
+
+void printHeap(){
+    for (int j = 1; j <= n; j++){
+        printf(" %d", H[j]);
+    }
+}
+
+void inPlaceHeapSort(){
+    for (int j = n; j >= 2; j--){
+        int tmp = H[j];
+        H[j] = H[1];
+        H[1] = tmp;
+        n--;
+        downHeap(1);
+    }
+    return;
+}
+int main(){
+    char command;
+    int key, N;
+    scanf("%d", &N);
+    n = N;
+    for (int i = 1; i <= N;i++){
+        scanf("%d", &key);
+        H[i] = key;
+    }
+
+    buildHeap();
+    inPlaceHeapSort();
+    n = N;
+    printHeap();
+    return 0;
+}
+
