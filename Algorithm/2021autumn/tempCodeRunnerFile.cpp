c13a052b4601c2d16d01cd21304ce66e4a872667
Looma1116 <jksjsmlove2@naver.com>
2021-09-28 20:19:14 +0900

    Asda

--- Algorithm/2021autumn/tempCodeRunnerFile.cpp
+++ Algorithm/2021autumn/tempCodeRunnerFile.cpp
@@ -1,1 +1,66 @@
-    while(L1!=NULL){
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int findPivot(int l,int r){
+    srand(time(NULL));
+    return (rand()%r+l);
+}
+
+int inPlacePartition(int **L,int l, int r, int k){
+    int *A = (*L);
+    int p = A[k];
+    int tmp;
+
+    tmp = A[k];
+    A[k] = A[r];
+    A[r] = tmp;
+
+    int i = l;
+    int j = r-1;
+    while(i<=j){
+        while(i<=j && A[i]<=p){
+            i++;
+        }
+        while(j>=i && A[j]>=p){
+            j--;
+        }
+        if(i<j){
+            tmp = A[i];
+            A[i] = A[j];
+            A[j] = tmp;
+        }
+    }
+    tmp = A[i];
+    A[i] = A[r];
+    A[r] = tmp;
+
+    return i;
+}
+
+void inPlaceQuickSort(int** L, int l, int r){
+    if(l<r){
+    int k = findPivot(l, r);
+    int i = inPlacePartition(L, l, r, k);
+    inPlaceQuickSort(L, l, i-1);
+    inPlaceQuickSort(L, i+1, r);
+    }
+}
+
+int main(){
+    int *A;
+    int n, data;
+    scanf("%d", &n);
+    A = (int *)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++){
+        scanf("%d", &data);
+        A[i] = data;
+    }
+
+    inPlaceQuickSort(&A, 0, n-1);
+    for (int i = 0; i < n; i++){
+        printf(" %d", A[i]);
+    }
+    return 0;
+}
--- Algorithm/2021autumn/week5_2.cpp
+++ Algorithm/2021autumn/week5_2.cpp
@@ -1,55 +1,66 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-typedef struct NODE{
-    int data;
-    NODE *right;
-} node;
-
-void getNODE(node** p){
-    *p = (node *)malloc(sizeof(node));
-    (*p)->right = NULL;
+int findPivot(int l,int r){
+    srand(time(NULL));
+    return rand()%(r-l+1)+l;
 }
 
-void addNODE(node **p, int data){
-    node *E, *R;
-    getNODE(&R);
+int inPlacePartition(int **L,int l, int r, int k){
+    int *A = (*L);
+    int p = A[k];
+    int tmp;
 
-    R->data = data;
-    E = (*p);
-    if((*p)==NULL){
-        (*p) = R;
-    }
-    else{
-        while(E->right!= NULL){
-            E = E->right;
+    tmp = A[k];
+    A[k] = A[r];
+    A[r] = tmp;
+
+    int i = l;
+    int j = r-1;
+    while(i<=j){
+        while(i<=j && A[i]<=p){
+            i++;
         }
-    E->right = R;
+        while(j>=i && A[j]>=p){
+            j--;
+        }
+        if(i<j){
+            tmp = A[i];
+            A[i] = A[j];
+            A[j] = tmp;
+        }
     }
+    tmp = A[i];
+    A[i] = A[r];
+    A[r] = tmp;
+
+    return i;
 }
 
-void printLIST(node* p,int L){
-    node *E;
-    getNODE(&E);
-    E = p;
-    for (int i = 0; i < L;i++){
-        printf(" %d",E->data);
-        E = E->right;
+void inPlaceQuickSort(int** L, int l, int r){
+    if(l<r){
+        int k = findPivot(l, r);
+        int i = inPlacePartition(L, l, r, k);
+        inPlaceQuickSort(L, l, i-1);
+        inPlaceQuickSort(L, i+1, r);
     }
 }
 
 int main(){
-    node* root;
+    int *A;
     int n, data;
-    root = NULL;
     scanf("%d", &n);
-
+    A = (int *)malloc(sizeof(int) * n);
     for (int i = 0; i < n; i++){
         scanf("%d", &data);
-        addNODE(&root, data);
+        A[i] = data;
     }
-    
-    printLIST(root, n);
+
+    inPlaceQuickSort(&A, 0, n-1);
+    for (int i = 0; i < n; i++){
+        printf(" %d", A[i]);
+    }
     return 0;
 }
