bf983e197ded90ac37c20ab102712d942af3ff2e
Looma1116 <jksjsmlove2@naver.com>
2021-12-05 20:33:07 +0900

    TopologicalSort 하는 중

--- Algorithm/2021autumn/final_training.cpp
+++ Algorithm/2021autumn/final_training.cpp
@@ -1,130 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node _node;
+typedef struct edge
+{
+    _node *from;
+    _node *to;
+    int weight;
+} edge;
+
 typedef struct node
 {
+    char value;
     node *next;
-    int key;
+    node *prev;
+    edge *header;
+    bool visited;
+    int in;
 } node;
 
-int hash(int, int);
+void getEdge(edge **);
 void getNode(node **);
-void addNode(node *, node *);
-void searchNode(node *, int);
-void deleteNode(node *, int);
-void printNode(node *);
+bool isEmpty(node *);
+void addNode(node *);
+void addEdge(node *, node *, int weight);
+
 int main()
 {
-    int M, key, *table;
-    node **list;
-    char c;
-    scanf("%d", &M);
-    getchar();
-    list = (node **)malloc(sizeof(node *) * M);
-    for (int i = 0; i < M; i++)
-    {
-        getNode(&list[i]);
-    }
-
-    scanf("%c", &c);
-    while (c != 'e')
-    {
-        if (c == 'i')
-        {
-            scanf("%d", &key);
-            node *p;
-            getNode(&p);
-            p->key = key;
-            addNode(list[hash(key, M)], p);
-        }
-        if (c == 's')
-        {
-            scanf("%d", &key);
-            searchNode(list[hash(key, M)], key);
-        }
-        if (c == 'd')
-        {
-            scanf("%d", &key);
-            deleteNode(list[hash(key, M)], key);
-        }
-        if (c == 'p')
-        {
-            for (int i = 0; i < M; i++)
-            {
-                printNode(list[i]);
-            }
-        }
-        getchar();
-        scanf("%c", &c);
-    }
     return 0;
 }
-int hash(int x, int M)
-{
-    return (x % M);
-}
 
+void getEdge(edge **e)
+{
+    (*e) = (edge *)malloc(sizeof(edge));
+    (*e)->from = NULL;
+    (*e)->to = NULL;
+}
 void getNode(node **p)
 {
+    edge *e;
+    getEdge(&e);
     (*p) = (node *)malloc(sizeof(node));
     (*p)->next = NULL;
+    (*p)->prev = NULL;
+    (*p)->header = e;
 }
 
-void addNode(node *head, node *add)
+bool isEmpty(node *Q)
 {
-    if (head->next == NULL)
-    {
-        head->next = add;
-        return;
-    }
-    add->next = head->next;
-    head->next = add;
-    return;
+    return (Q->next == NULL);
 }
-
-void searchNode(node *head, int key)
+void addNode(node *Q)
 {
-    node *tmp = head;
-    int i = 0;
+    node *tmp;
+    tmp = Q;
     while (tmp != NULL)
     {
-        if (tmp->key == key)
-        {
-            printf("%d\n", i);
-            return;
-        }
-        tmp = tmp->next;
-        i++;
-    }
-    printf("0\n");
-    return;
-}
-
-void deleteNode(node *head, int key)
-{
-    node *tmp = head->next;
-    int i = 1;
-    while (tmp != NULL)
-    {
-        if (tmp->next->key == key)
-        {
-            printf("%d\n", i);
-            tmp->next = tmp->next->next;
-            return;
-        }
-        i++;
-        tmp = tmp->next;
-    }
-    printf("0\n");
-    return;
-}
-
-void printNode(node *head)
-{
-    node *tmp = head;
-    while (tmp != NULL)
-    {
-        printf(" %d", tmp->key);
-        tmp = tmp->next;
     }
 }
+void addEdge(node *, node *, int weight);
